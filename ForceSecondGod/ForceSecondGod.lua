4e2fd890274489b5769ce7d11405acd90418d868
SleepSoul <spider.coopplay@gmail.com>
2023-01-14 00:05:27 +0000

    feat(ForceSecondGod): Add ability to force keepsake god
    Previously if your second god was the same as the keepsake you started with, nothing would be forced. This refactors the logic to allow a god to be forced twice.

--- ForceSecondGod/ForceSecondGod.lua
+++ ForceSecondGod/ForceSecondGod.lua
@@ -6,54 +6,64 @@
     Force a second god to appear by the point of Tartarus midboss, configurable per aspect.
 ]]
 ModUtil.Mod.Register("ForceSecondGod")
-ForceSecondGod.IsGodKeepsake = {
-    ForceZeusBoonTrait = true,
-    ForcePoseidonBoonTrait = true,
-    ForceAphroditeBoonTrait = true,
-    ForceArtemisBoonTrait = true,
-    ForceDionysusBoonTrait = true,
-    ForceAthenaBoonTrait = true,
-    ForceAresBoonTrait = true,
-    ForceDemeterBoonTrait = true,
+
+ForceSecondGod.GodKeepsakes = {
+    ForceZeusBoonTrait = "ZeusUpgrade",
+    ForcePoseidonBoonTrait = "PoseidonUpgrade",
+    ForceAphroditeBoonTrait = "AphroditeUpgrade",
+    ForceArtemisBoonTrait = "ArtemisUpgrade",
+    ForceDionysusBoonTrait = "DionysusUpgrade",
+    ForceAthenaBoonTrait = "AthenaUpgrade",
+    ForceAresBoonTrait = "AresUpgrade",
+    ForceDemeterBoonTrait = "DemeterUpgrade",
 }
 
-ModUtil.Path.Wrap( "SetupRoomReward", function( baseFunc, currentRun, room, previouslyChosenRewards, args )
+function ForceSecondGod.IsGodForceEligible( room, godToForce, keepsakeGod, keepsakeCharges, previousOffers )
+    if room.ChosenRewardType ~= "Boon"
+    or not room.IsMiniBossRoom
+    or Contains( previousOffers, godToForce )
+    or keepsakeCharges > 0 and not Contains( previousOffers, keepsakeGod )
+    or godToForce == keepsakeGod and CurrentRun.LootTypeHistory[godToForce] >= 2
+    or godToForce ~= keepsakeGod and CurrentRun.LootTypeHistory[godToForce] >= 1
+    or ReachedMaxGods()
+    then
+        return false
+    end
     
+    return true
+end
+
+ModUtil.Path.Wrap( "SetupRoomReward", function( baseFunc, currentRun, room, previouslyChosenRewards, args )
     args = args or {}
 
     CheckPreviousReward( currentRun, room, previouslyChosenRewards, args )
 
-    if room.ChosenRewardType == "Boon" then
-        local currentAspect = RCLib.GetAspectName()
-        local godToForce = RCLib.EncodeBoonSet(ForceSecondGod.config.AspectSettings[currentAspect])
-        local keepsakeCharges = 0
-        local isMiniboss = room.IsMiniBossRoom or false
-        local excludeLootNames = {}
-		if previouslyChosenRewards ~= nil then -- Same vanilla code that prevents duplicate gods
-			for i, data in pairs( previouslyChosenRewards ) do
-				if data.RewardType == "Boon" then
-					table.insert( excludeLootNames, data.ForceLootName )
-				end
-			end
-		end
+    local currentAspect = RCLib.GetAspectName()
+    local godToForce = RCLib.EncodeBoonSet(ForceSecondGod.config.AspectSettings[currentAspect])
 
-        if ForceSecondGod.IsGodKeepsake[GameState.LastAwardTrait] then
-            for k, data in ipairs(CurrentRun.Hero.Traits) do
-                if data.Name == GameState.LastAwardTrait and data.Uses then
-                    keepsakeCharges = data.Uses
-                end
+    local excludeLootNames = {}
+    if previouslyChosenRewards ~= nil then -- Same vanilla code that prevents duplicate gods
+        for i, data in pairs( previouslyChosenRewards ) do
+            if data.RewardType == "Boon" then
+                table.insert( excludeLootNames, data.ForceLootName )
             end
-        end
-        
-        ForceSecondGod.DumpExcluded = excludeLootNames
-        if not CurrentRun.LootTypeHistory[godToForce]
-        and isMiniboss 
-        and keepsakeCharges <= 0
-        and not Contains(excludeLootNames, godToForce) then
-            room.ForceLootName = godToForce
         end
     end
     
+    local keepsakeCharges = 0
+    local keepsakeGod = ForceSecondGod.GodKeepsakes[GameState.LastAwardTrait] or nil
+    if keepsakeGod then
+        for k, data in ipairs(CurrentRun.Hero.Traits) do
+            if data.Name == GameState.LastAwardTrait and data.Uses then
+                keepsakeCharges = data.Uses
+            end
+        end
+    end
+
+    if ForceSecondGod.IsGodForceEligible( room, godToForce, keepsakeGod, keepsakeCharges, excludeLootNames ) then
+        room.ForceLootName = godToForce
+    end
+
     baseFunc( currentRun, room, previouslyChosenRewards, args )
 end, ForceSecondGod)
 
